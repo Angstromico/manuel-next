c70341dabe245ce7d3683546fc1ab4e4f2457ffb
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2024-11-09 17:44:29 -0400

    More gap between projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -11,9 +11,9 @@ const Projects = () => {
         <h2 className='text-center xl:text-left text-3xl md:text-5xl font-semibold text-transparent bg-clip-text bg-gradient-dark dark:bg-gradient-light py-20'>
           {t('title')}
         </h2>
-        <div className='h-full w-screen flex flex-col xl:flex-row gap-20 xl:gap-5 px-10'>
+        <div className='h-full w-screen flex flex-col xl:flex-row gap-16 xl:gap-5 px-10'>
           {/* Adding a wrapper div with grid at xl breakpoint */}
-          <div className='w-full xl:grid xl:grid-cols-3 gap-16 xl:gap-5'>
+          <div className='w-full grid grid-cols-1 xl:grid-cols-3 gap-16 xl:gap-5'>
             <ProjectCard
               src='/mia.png'
               title='MIA Femtech'
