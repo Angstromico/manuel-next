0bf1addbfd4450f6611f5c27b745e45f4c835349
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2024-11-10 20:33:16 -0400

    Socials links in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,6 @@
 import { useTranslations } from 'next-intl'
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
+import { SiGmail } from 'react-icons/si'
 
 export default function Footer() {
   const t = useTranslations('Footer')
@@ -6,9 +8,38 @@ export default function Footer() {
 
   return (
     <footer className='my-10 text-center'>
-      <p>
+      <p className='mb-4'>
         {currentYear} © {t('copyright')}
       </p>
+      <div className='flex justify-center gap-6'>
+        <a
+          href='mailto:manuesteban1990@gmail.com'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='social-icon'
+          aria-label='Twitter'
+        >
+          <SiGmail />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/manuel-esteban-morales-zuarez-68573b189/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='social-icon'
+          aria-label='LinkedIn'
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href='https://github.com/Angstromico'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='social-icon'
+          aria-label='GitHub'
+        >
+          <FaGithub />
+        </a>
+      </div>
     </footer>
   )
 }
